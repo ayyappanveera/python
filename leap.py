ef6ba50b17ca52fe405cdf6e61ea11c1c6c226e7
ayyappanveera <53147833+ayyappanveera@users.noreply.github.com>
2019-07-21 23:06:24 +0530

    Update leap.py

--- leap.py
+++ leap.py
@@ -1,11 +1,5 @@
-x = int(input())
-if (x % 4) == 0:  
-  if (x % 100) == 0:  
-    if (x % 400) == 0:  
-      print("yes")  
-    else:  
-      print("no")  
-  else:  
-    print("yes")  
-else:  
-print("no") 
+l=int(input())
+if(l%4==0):
+  print("yes")
+else:
+  print("no")
